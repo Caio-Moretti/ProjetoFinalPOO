ccdb1508ae001a20d25a7a1d2859ad95c418467d
Caio Moretti de Macedo <caiomoretti7@gmail.com>
2022-09-27 00:41:54 -0300

    Finishing the first beta version of the project.

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+//TEMÁTICA: MINHA GARAGEM (Carros e Motos)
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,22 +15,15 @@ public class Principal {
 
     public static void main(String[] args) {
 
-//        try {
-//            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-//            writer.write("funcionou");
-//            writer.close();
-//        } catch(IOException e){
-//            e.printStackTrace();
-//        }
-
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("entradas/entrada1.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("entrada1.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("resultado1.txt")); // Define uma pasta txt a ser criada
             String l;
-            ArrayList<Carro> garagemCarros = new ArrayList<Carro>();
+            ArrayList<Carro> garagemCarros = new ArrayList<Carro>(); // ArrayList que recebe os carros do arquivo entrada1.txt
             for (int i = 0; i < 4; i++) {
                 l = reader.readLine();
                 if (l == null) break;
-                ArrayList<String> info = new ArrayList<String>(Arrays.asList(l.split(",")));
+                ArrayList<String> info = new ArrayList<String>(Arrays.asList(l.split(","))); // ArrayList para separar as palavras que estão no arquivo (linha por linha)
                 if(i==0) {
                     CarroEsportivo carroDoAguiar = new CarroEsportivo(info.get(0), Integer.parseInt(info.get(1)), info.get(2), info.get(3), Double.parseDouble(info.get(4)));
                     garagemCarros.add(carroDoAguiar);
@@ -44,7 +38,17 @@ public class Principal {
                     garagemCarros.add(carroReservaCaio);
                 }
             }
-            System.out.println(garagemCarros);
+            int anoCarro0 = garagemCarros.get(0).getAno();
+            int anoCarro1 = garagemCarros.get(1).getAno();
+            int anoCarro2 = garagemCarros.get(2).getAno();
+            int anoCarro3 = garagemCarros.get(3).getAno();
+            int media = (anoCarro0 + anoCarro1 + anoCarro2 + anoCarro3)/4;
+            writer.write(String.format("Ano de Fabricação do primeiro carro: %d" +
+                    "\nAno de Fabricação do segundo carro: %d" +
+                    "\nAno de Fabricação do terceiro carro: %d" +
+                    "\nAno de Fabricação do quarto carro: %d" +
+                    "\nMédia de idade entre os carros: %d",anoCarro0, anoCarro1, anoCarro2, anoCarro3, media)); // Escreve na pasta resultado1.txt
+            writer.close();
             reader.close();
 
         } catch (Exception e) {
@@ -52,10 +56,11 @@ public class Principal {
         }
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("entradas/entrada2.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("entrada2.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("resultado2.txt")); // Define uma pasta txt a ser criada
             String linha;
             ArrayList<Moto> garagemMotos = new ArrayList<Moto>();
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) { // Refactor: mudar o limite do for para o len de entrada.txt #PROBLEMA = o programa precisaria descobrir qual é esportiva e qual é passeio(PESQUISAR PELA DOCUMENTAÇÃO DO READER)
                 linha = reader.readLine();
                 if (linha == null) break;
                 ArrayList<String> info = new ArrayList<String>(Arrays.asList(linha.split(",")));
@@ -73,11 +78,25 @@ public class Principal {
                     garagemMotos.add(motoReservaCaio);
                 }
             }
-            System.out.println(garagemMotos);
+            int anoMoto0 = garagemMotos.get(0).getAno();
+            int anoMoto1 = garagemMotos.get(1).getAno();
+            int anoMoto2 = garagemMotos.get(2).getAno();
+            int anoMoto3 = garagemMotos.get(3).getAno();
+            int maiorAno = 0;
+            for (int i = 0; i < 4; i++){
+                if (garagemMotos.get(i).getAno() > maiorAno) {
+                    maiorAno = garagemMotos.get(i).getAno();
+                }
+            }
+            writer.write(String.format("Ano de Fabricação da primeira moto: %d" +
+                    "\nAno de Fabricação da segunda moto: %d" +
+                    "\nAno de Fabricação da terceira moto: %d" +
+                    "\nAno de Fabricação da quarta moto: %d" +
+                    "\nAno de fabricação da moto mais nova: %d",anoMoto0, anoMoto1, anoMoto2, anoMoto3, maiorAno));// Escreve na pasta resultado2.txt
+            writer.close();
             reader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
